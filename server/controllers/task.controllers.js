43ea9684c2cb3ddba67a146192498467475a6b34
JoacoMaurtua <joacomaurtua@gmail.com>
2021-09-23 20:58:25 -0500

    update task function un controllers implemented

--- server/controllers/task.controllers.js
+++ server/controllers/task.controllers.js
@@ -7,7 +7,7 @@ const findTasks = (req,res) =>{
          res.json({error:error, message:'Tasks not found'})
          res.sendStatus(404)
        })
-}
+};
 
 
 const findSingleProduct = (req,res) =>{
@@ -18,18 +18,28 @@ const findSingleProduct = (req,res) =>{
           res.sendStatus(404);
         })
 
-}
+};
 
 const createTask = (req,res) =>{
   Task.create(req.body)
       .then(results => res.json({data:results}))
       .catch(error=>{
-        res.json({error:error, message:'Task not found'})
+        res.json({error:error, message:'Could not create a task'})
         res.sendStatus(500);
       })
+};
+
+const updateTask = (req,res) =>{
+    Task.findOneAndUpdate({_id:req.params.id}, req.body, {new:true})
+    .then(results => res.json({data:results}))
+      .catch(error=>{
+        res.json({error:error, message:'Could not create a task'})
+        res.sendStatus(500);
+      })
+
 }
 
 
 
 
-module.exports = {findTasks, findSingleProduct, createTask}
+module.exports = {findTasks, findSingleProduct, createTask, updateTask}
