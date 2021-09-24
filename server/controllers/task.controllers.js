673d6fa600e71a2fcb7dc90eb10b26dd87e85c28
JoacoMaurtua <joacomaurtua@gmail.com>
2021-09-23 20:44:45 -0500

    find one task function in controllers

--- server/controllers/task.controllers.js
+++ server/controllers/task.controllers.js
@@ -10,4 +10,14 @@ const findTasks = (req,res) =>{
 }
 
 
-module.exports = {findTasks}
+const findSingleProduct = (req,res) =>{
+    Task.findOne({_id:req.params.id})
+        .then(results => res.json({data:results}))
+        .catch(error=>{
+          res.json({error:error, message:'Task not found'})
+          res.sendStatus(404);
+        })
+
+}
+
+module.exports = {findTasks, findSingleProduct}
