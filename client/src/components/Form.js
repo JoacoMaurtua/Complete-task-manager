38208c6d9608b44055eee8cf2f71ebb1643cc297
JoacoMaurtua <joacomaurtua@gmail.com>
2021-09-27 17:50:37 -0500

    handle onChange function

--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -1,12 +1,27 @@
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 import {DateTimePicker} from '@material-ui/pickers';
+import {Link} from 'react-router-dom';
 
 export default function Form({create,update}) {
 
   const [dateSelected, setDateSelected] = useState(new Date());
+  console.log(dateSelected);
 
-  console.log(dateSelected)
+  const [taskInput, setTaskInput] = useState({
+    title:'',
+    description:''
+  });
+
+  const handleOnChange =e=>{
+    const {name,value} = e.target;
+    setTaskInput({
+      ...taskInput,
+      [name]:value
+    })
+  };
+
+  const {title,description} = taskInput;
 
   return (
     <div className="formContainer">
@@ -16,26 +31,36 @@ export default function Form({create,update}) {
       <form>
         <label htmlFor="title"Title>Title: </label>
         <input
-         
+          name="title"
+          value={title}
           id="title"
-          type="text"        
+          type="text"
+          onChange={handleOnChange}        
         />
 
         <label htmlFor='date'>Date: </label>
         <DateTimePicker className="datePicker"
+          name="date"
           style={{marginBottom:'3rem', color:'white'}} 
           value={dateSelected}
           onChange={setDateSelected}
         />
 
-        <label htmlFor='texttarea'>Descripcion: </label>
+        <label htmlFor='textarea'>Description: </label>
         <textarea
-        
+            name="description"
+            value={description}
             id="textarea"
             cols='30' rows='7.5'
+            onChange={handleOnChange}   
         ></textarea>
 
         <button className = "taskButton" type="submit">Crear tarea</button>
+
+        <Link>
+          <p>Ver todas las tareas</p>
+        </Link>
+        
 
       </form>
       
