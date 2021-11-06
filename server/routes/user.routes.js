552c5a60b674f020d53ba20b09d86541ee82536a
JoacoMaurtua <joacomaurtua@gmail.com>
2021-11-05 22:41:33 -0500

    User routes implemented

---
+++ server/routes/user.routes.js
@@ -0,0 +1,25 @@
+const express = require("express");
+
+const router = express();
+
+const {findUsers,findSingleUser,createUser,updateUser, deleteUser} = require("../controllers/user.controllers");
+
+//Rutas:
+
+//encontrar todos los usuarios
+router.get('/users',findUsers);
+
+//encontrar un usuarios
+router.get('/users/:id', findSingleUser);
+
+//crear un usuario
+router.post('/users/create',createUser);
+
+//actualizar un usuario
+router.put('/users/update/:id', updateUser);
+
+//eliminar un usuario
+router.delete('/users/delete/:id', deleteUser);
+
+module.exports = router;
+
