95b6f60d61e5b4f0edbdd88d12d76f41054bc301
Stefano <piero.quiroz93@gmail.com>
2022-01-20 21:21:28 -0500

    Credenciales de seguridad

--- server/config/jwt.config.js
+++ server/config/jwt.config.js
@@ -6,8 +6,8 @@ const secretKey = process.env.SECRET_KEY;
 //si la tenemos autoriza sino no autoriza
 const authenticate = (req, res, next) => {
     //para saber la cookie userToken (demostrativo)
-    console.log({headers: req.headers.authorization})
-    console.log({cookies:req.cookies.usertoken});
+    //console.log({headers: req.headers.authorization})
+    console.log(req.cookies.usertoken ? req.cookies.usertoken : "Problema con los credenciales");
     jwt.verify(req.cookies.usertoken, secretKey, (err,payload) =>{
         if(err){
             //401 => unauthorized sino se encuentra la cookie reporta un 401
